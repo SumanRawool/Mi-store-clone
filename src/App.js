c8ae594f92de4fe449ebc719020add200ded3ba2
Suman <sumanrawool2@gmail.com>
2023-04-24 21:24:21 +0530

    First corousal added to the website

--- src/App.js
+++ src/App.js
@@ -2,11 +2,14 @@ import "./App.css";
 import PreNavbar from "./components/PreNavbar";
 import Navbar from "./components/Navbar.js";
 import { BrowserRouter as Router } from "react-router-dom";
+import Slider from "./components/Slider.js";
+import data from "./data/data.json";
 function App() {
   return (
     <Router>
       <PreNavbar />
       <Navbar />
+      <Slider start={data.banner.start} />
     </Router>
   );
 }
