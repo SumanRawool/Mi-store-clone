8346cee35676420a2a19be2f4adc82fce69dfed3
Suman <sumanrawool2@gmail.com>
2023-04-25 13:28:58 +0530

    HotAccessoried section is created

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import "./App.css";
 import PreNavbar from "./components/PreNavbar";
 import Navbar from "./components/Navbar.js";
-import { BrowserRouter as Router } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Slider from "./components/Slider.js";
 import data from "./data/data.json";
 import Offers from "./components/Offers.js";
 import Heading from "./components/Heading.js";
+import StarProduct from "./components/StarProduct.js";
+import HotAccessoriesMenu from "./components/HotAccessoriesMenu.js";
+import HotAccessories from "./components/HotAccessories.js";
+
 function App() {
   return (
     <Router>
@@ -14,6 +18,67 @@ function App() {
       <Slider start={data.banner.start} />
       <Offers offer={data.offer} />
       <Heading text="STAR PRODUCTS" />
+      <StarProduct starProduct={data.starProduct} />
+      <Heading text="HOT ACCESSORIES" />
+      <HotAccessoriesMenu />
+      {/* <Routes>
+        <Route exact path="/music">
+          <HotAccessories
+            music={data.hotAccessories.music}
+            musicCover={data.hotAccessoriesCover.music}
+          />
+        </Route>
+      </Routes> */}
+      <Routes>
+        <Route
+          exact
+          path="/music"
+          element={
+            <HotAccessories
+              music={data.hotAccessories.music}
+              musicCover={data.hotAccessoriesCover.music}
+            />
+          }
+        />
+        <Route
+          path="/smartDevice"
+          element={
+            <HotAccessories
+              smartDevice={data.hotAccessories.smartDevice}
+              smartDeviceCover={data.hotAccessoriesCover.smartDevice}
+            />
+          }
+        />
+        <Route
+          path="/home"
+          element={
+            <HotAccessories
+              home={data.hotAccessories.home}
+              homeCover={data.hotAccessoriesCover.home}
+            />
+          }
+        />
+        <Route
+          path="/lifestyle"
+          element={
+            <HotAccessories
+              lifeStyle={data.hotAccessories.lifeStyle}
+              lifeStyleCover={data.hotAccessoriesCover.lifeStyle}
+            />
+          }
+        />
+        <Route
+          path="/mobileAccessories"
+          element={
+            <HotAccessories
+              mobileAccessories={data.hotAccessories.mobileAccessories}
+              mobileAccessoriesCover={
+                data.hotAccessoriesCover.mobileAccessories
+              }
+            />
+          }
+        />
+      </Routes>
     </Router>
   );
 }
