8df6ef87a35cc53e896261dc9775ffe878d98f0d
Suman <sumanrawool2@gmail.com>
2023-04-25 11:15:31 +0530

    Heading component created

--- src/App.js
+++ src/App.js
@@ -4,12 +4,16 @@ import Navbar from "./components/Navbar.js";
 import { BrowserRouter as Router } from "react-router-dom";
 import Slider from "./components/Slider.js";
 import data from "./data/data.json";
+import Offers from "./components/Offers.js";
+import Heading from "./components/Heading.js";
 function App() {
   return (
     <Router>
       <PreNavbar />
       <Navbar />
       <Slider start={data.banner.start} />
+      <Offers offer={data.offer} />
+      <Heading text="STAR PRODUCTS" />
     </Router>
   );
 }
